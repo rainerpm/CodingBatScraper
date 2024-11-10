4eb5a6d72ecc9ba2d5ac55591ee5eb7fd536fa6d
Rainer Mueller <rainer.mueller@austinisd.org>
2024-11-10 07:40:43 -0600

    moved from CBScrape.py

---
+++ CBScrapedata.py
@@ -0,0 +1,50 @@
+# CONSTANTS to setup
+# set below to True if you only want output from specific class period(s)
+# You will have to update the memoParse() function below to extract
+# the class period info from the memo field on the CodingBat report page
+
+# If you choose to extract files you'll also need to setup these
+DEFAULTOUTPUTDIR = "C:/Users/E151509/Desktop/misc/CodingBatResults"
+MOSSDIR = "C:/Users/E151509/My Drive/My LASA/misc/tools/moss plagiarize checking"
+ONLINESOLUTIONS = "C:/Users/E151509/My Drive/My LASA/CodingBat/Plagiarism/"
+SCOREBOARDDIR = r'C:\Users\E151509\My Drive (rainer.mueller@austinisd.org)\CSAC\StudentSubmissions\2024-25\scoreboard\CodingBat'
+
+
+PERIODSPYTHON = ["P3","P5","P6","P8"]
+PERIODSJAVA = []
+
+###### JAVA ######
+assignments_java = {}
+#assignments_java["CodingBat String Exercises"] = {"p161056" : "makeAbba", "p147483" : "makeTags","p184030" : "makeOutWord","p108853" : "extraEnd","p172267" : "firstHalf","p130896" : "withoutEnd","p143825" : "nonStart","p197720" : "left2","p137729" : "middleTwo","p174148" : "nTwice","p115863" : "middleThree","p194786" : "lastTwo"}
+#assignments_java["Aa-Logic80%"] = {"p159531" : "cigarParty","p103360":"dataFashion","p141061":"squirrelPlay","p160543":"alarmClock","p193613":"nearTen","p110973":"answerCell","p153748":"shareDigit"}
+#assignments_java["Ab-Logic90%"] = {"p177181":"teaParty","p137136":"fizzString","p115243":"fizzString2","p113261":"twoAsOne"}
+#assignments_java["Ac-Logic100%"] = {"p154188":"inOrder","p140272":"inOrderEqual","p169213":"lastDigit","p179196":"lessBy10","p115384":"maxMod5"}
+#assignments_java["CodingBat Logic Exercises"] = {"p159531" : "cigarParty","p103360":"dataFashion","p141061":"squirrelPlay","p160543":"alarmClock","p193613":"nearTen","p110973":"answerCell","p153748":"shareDigit","p177181":"teaParty","p137136":"fizzString","p115243":"fizzString2","p113261":"twoAsOne","p154188":"inOrder","p140272":"inOrderEqual","p169213":"lastDigit","p179196":"lessBy10","p115384":"maxMod5"}
+# assignments_java["Ba-Arrays80%"] = {"p167011":"makePi","p101230":"makeEnds","p171022":"has23",
+#                                     "p111327":"sum67", "p180920":"fizzArray", "p159979":"modThree",
+#                                     "p105031":"shiftLeft", "p199484":"tenRun"}
+# assignments_java["Bb-Arraysjust100%"] = {"p189576":"maxSpan","p158767":"canBalance","p104090":"seriesUp"}
+# assignments_java["Bb-Arrays80%and100%"] = {"p167011":"makePi","p101230":"makeEnds","p171022":"has23",
+#                                     "p111327":"sum67", "p180920":"fizzArray", "p159979":"modThree",
+#                                     "p105031":"shiftLeft", "p199484":"tenRun",
+#                                     "p189576":"maxSpan","p158767":"canBalance","p104090":"seriesUp"}
+#assignments_java["CodingBat Array Exercises"] = {"p167011":"makePi","p101230":"makeEnds","p171022":"has23","p111327":"sum67","p180920":"fizzArray","p159979":"modThree","p105031":"shiftLeft","p199484":"tenRun","p189576":"maxSpan","p158767":"canBalance","p104090":"seriesUp"}
+#assignments_java["CodingBat LASA Drills 01"] = {"p265283" : "LASA_sameClass","p277511":"LASA_borderDigits","p224017":"LASA_dnaInversion","p272927":"LASA_startWorking","p231264":"LASA_crossbarCalc"}
+
+#assignments_java["Challenge 1"] = {"p271468":"myParseInt","p287839":"noRepeatSubSeq"}
+#assignments_java["Midterm"] = {"p283631" : "isAlphaNumeric", "p292285" : "whatFloor"}
+#assignments_java["CodingBat Recursion OLD"] = {"p194781":"triangle","p163932":"sumDigits","p120015":"fibonacci","p118182":"strCopies"}
+#assignments_java["CodingBat Recursion"] = {"p194781":"triangle","p163932":"sumDigits","p120015":"fibonacci","p118182":"strCopies"}
+
+      
+###### PYTHON ######
+assignments_python = {}
+assignments_python["CodingBat Warmup"] = {"p166884" : "parrot_trouble", "p124984" : "makes10", "p124676" : "near_hundred", "p162058" : "pos_neg"}
+assignments_python["CodingBat Logic"] = {"p129125" : "date_fashion", "p135815" : "squirrel_play", "p119867" : "alarm_clock"}
+assignments_python["CodingBat Berlin Wall"] = {"p225759" : "berlinWall0", "p215725" : "berlinWall1", "p212912" : "berlinWall2"}
+#assignments_python["CodingBat Strings1"] = {"p115413" : "hello_name","p182144" : "make_abba","p132290" : "make_tags","p129981" : "make_out_word","p148853" : "extra_end","p184816" : "first_two","p107010" : "first_half","p138533" : "without_end","p194053" : "combo_string","p127703" : "non_start","p160545" : "left2"}
+#assignments_python["CodingBat List Bonus"] = {"p192962":"reverse3","p135290":"max_end3","p126968":"centered_average","p108886":"sum67"}
+#assignments_python["rightRange"] = {"p299949" : "rightRange"}
+#assignments_python["Final 2021 - Day 1"] = {"p255003" : "div2and7","p281953" : "allAboutListsPart1"}  
+#assignments_python["Final 2021 - Day 2"] = {"p226557" : "allAboutListsPart2","p217373" : "allAboutListsPart3"}
+
